7f58b47fb5411c7c8cde8fa80f283365fc50f9ed
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-20 20:40:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,11 +2,10 @@
 
 def countdown(number)
   while number > 0
-    puts "#{number} SECOND(S)!"
+    ss
     number -= 1
     sleep(1)
   end
-  puts "HAPPY NEW YEAR!"
   return "HAPPY NEW YEAR!"
 end
 
