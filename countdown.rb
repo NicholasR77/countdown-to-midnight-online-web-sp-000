5e4ca0a845d9ee50e4d7f6660b7edfdbd8935e21
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-20 20:47:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,22 +1,26 @@
 #write your code here
 
-def countdown(int)
-  while int > 0
-    puts "#{int} SECOND(S)!"
-    int -= 1
-  end
-  puts "HAPPY NEW YEAR!"
-  return "HAPPY NEW YEAR!"
+def countdown(num)
+
+while num > 0
+
+  puts "#{num} SECOND(S)!"
+  num -= 1
+
+end
+  "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(number)
-  while number > 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
-    sleep(1)
-  end
-  puts "HAPPY NEW YEAR!"
-  return "HAPPY NEW YEAR!"
-end
 
-countdown(10)
+def countdown_with_sleep(num)
+
+while num > 0
+
+  puts "#{num} SECOND(S)!"
+  sleep(1)
+  num -= 1
+
+end
+  "HAPPY NEW YEAR!"
+end
+end
